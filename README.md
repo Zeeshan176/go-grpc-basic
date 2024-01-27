7995cf80bc785e84a1483cd40223c777bd14d18f
Zeeshan176 <zeeshanmd956@gmail.com>
2024-01-27 15:32:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gRpc Basic with Golang
+
+This project is used for learning gRpc with golang
+
+## Installation
+
+Make sure you have Go installed on your machine. Clone the repository and run:
+
+```bash
+go get -u github.com/Zeeshan176/go-grpc-basic
+
+# go to server folder than run
+go run . *.go
+
+# go to client folder than run
+go run . *.go
+
+# comment out one by one this [callSayHello(), callSayHelloServerStream(), callSayHelloClientStream(),callSayHelloBidirectionalStream()] than check your rpc types
---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/Zeeshan176/go-grpc-basic/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	// pb means protobuf stuff
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
